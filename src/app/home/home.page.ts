20d029e4e2406b13a65d216fe10be460f85c7e1e
Leonardo Gigliotti <leonardosilvagigliottii@gmail.com>
2023-06-06 22:01:47 -0300

    Finalizando o camApp
    Finalizando o camApp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { actionSheetController } from '@ionic/core';
+import { Foto } from '../models/Foto.interface';
+import { FotoService } from '../services/foto.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  constructor(public fotoService: FotoService, public actionSheetController: ActionSheetController) { }
+  async ngOnInit(){
+    await this.fotoService.carregarFotosSalvas();
+} 
+public async showActionSheet(foto: Foto, position: number) {
+  const actionSheet = await this.actionSheetController.create({
+    header: 'Fotos',
+    buttons: [{
+      text: 'Delete',
+      role: 'destructive',
+      icon: 'trash',
+      handler: () => {
+        this.fotoService.deletePicture(foto, position);
+      }
+    }, {
+      text: 'Cancelar',
+      icon: 'close',
+      role: 'cancel',
+      handler: () => {
+        //nothing to do, action sheet is automatically closed
+      }
+    }]
+  });
+  await actionSheet.present();
 }
+
+tirarFoto() {
+  this.fotoService.tirarFoto();
+}
+}
+
